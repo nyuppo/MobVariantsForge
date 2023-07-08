83f9659a53b15ef03682cf00bd201f868b3d8821
nyuppo <thisisnyuppo@gmail.com>
2023-07-08 01:29:53 -0400

    Fixed a bug that prevented servers from starting

---
+++ src/main/java/com/github/nyuppo/client/RenderLayerEventHandler.java
@@ -0,0 +1,31 @@
+package com.github.nyuppo.client;
+
+import com.github.nyuppo.MoreMobVariants;
+import com.github.nyuppo.client.renderer.entity.layers.PigMudLayer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.function.Function;
+
+@Mod.EventBusSubscriber(modid = MoreMobVariants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class RenderLayerEventHandler {
+    @SubscribeEvent
+    public static void addLayers(EntityRenderersEvent.AddLayers event) {
+        addLayerToRenderer(event, EntityType.PIG, PigMudLayer::new);
+    }
+
+    // Thanks gigaherz
+    private static <T extends LivingEntity, R extends LivingEntityRenderer<T, M>, M extends EntityModel<T>> void addLayerToRenderer(EntityRenderersEvent.AddLayers event, EntityType<T> entityType, Function<R, ? extends RenderLayer<T,M>> factory)
+    {
+        R renderer = event.getRenderer(entityType);
+        if (renderer != null) renderer.addLayer(factory.apply(renderer));
+    }
+}
